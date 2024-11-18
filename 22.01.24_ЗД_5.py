8bd9336f4d880fbcf849963d72eb734983461541
SuperGT <122013461+SuperGT911@users.noreply.github.com>
2024-11-18 19:36:41 +0300

    Delete 22.01.24_ЗД_5.py

--- 22.01.24_ЗД_5.py
+++
@@ -1,8 +0,0 @@
-# Задача №5 Пирожки | Целочисленное деление.
-a, b, n = map(int, input().split())
-
-total = (a * 100 + b) * n
-rub = total // 100 # целая часть - рубли
-kop = total % 100 # остаток - копейки
-
-print(f"{rub}rub. {kop}kop.")
